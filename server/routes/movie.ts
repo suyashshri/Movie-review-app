7b8cf899c06a704ac6171366ea88cea31d4e8de9
suyashshri <suyashshrivastava736@gmail.com>
2024-02-12 18:59:24 +0530

    Completing Server code

--- server/routes/movie.ts
+++ server/routes/movie.ts
@@ -41,6 +41,7 @@ router.post("/", async (req: Request, res: Response) => {
     );
     res.status(200).json({
       message: "Movie Inserted Successfully!",
+      data: results.rows[0],
     });
   } catch (err) {
     console.log(err);
@@ -48,13 +49,37 @@ router.post("/", async (req: Request, res: Response) => {
 });
 
 //Update movie
-router.put("/:id", (req: Request, res: Response) => {
-  const id = req.params.id;
+router.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const { title, genres, imdb, year } = req.body;
+    const id = req.params.id;
+    const results = await db.query(
+      "UPDATE movies SET title =$1, genres = $2, imdb= $3, year = $4 where id = $5 returning *",
+      [title, genres, imdb, year, id]
+    );
+    res.status(200).json({
+      message: "Movie Updated Successfully!",
+      data: results.rows,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //delete movie
-router.delete("/:id", (req: Request, res: Response) => {
-  const id = req.params.id;
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const results = await db.query(
+      "DELETE from movies where id = $1 returning *",
+      [id]
+    );
+    res.status(200).json({
+      message: "Movie Deleted Successfully!",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default router;
